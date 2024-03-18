77a5265121c1ced7a7d73a8db12d05e761c4e562
adhithyaa <adhthyaa.mv@kalvium.community>
2024-03-18 10:14:24 +0530

    created-navbar

--- frontend/VibeVault/src/App.jsx
+++ frontend/VibeVault/src/App.jsx
@@ -1,36 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import "./App.css"
 
-
-
-
-function App() {
-  const [count, setCount] = useState(0)
-
+function Navbar() {
   return (
-    <>
-
-    <div className='main'><h1>VIBE VAULT</h1></div>
-    <div className='about'>
-    Introducing Vibe Vault, a unique project designed to enhance your entertainment experience. 
-    Users simply select their current mood, and are seamlessly guided to a subsequent page where they can choose between music, movies, or web series. 
-    The platform then prompts users to specify their language preference, presenting curated options tailored to their selections. 
-    Dive into personalized entertainment with Vibe Vault and let your mood guide the way!"
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Vibe Vault</h1>
+      <div className="navbar">
+        <a href="#">Home</a>
+        <a href="#">Login</a>
+        <a href="#">Sign Up</a>
+        <div className="search-container">
+          <input type="text" placeholder="Search.." />
+        </div>
+      </div>
     </div>
-    
-    <div className='wrapper'>
-      <form action="">
-          <h1>Login</h1>
-          <div className='input-box'>
-            <input type="text" placeholder='Username' />
-            <input type="password" placeholder='password' />
-
-          </div>
-      </form>
-    </div>
-    
-    </>
-  )
+  );
 }
 
-export default App
+export default Navbar;
