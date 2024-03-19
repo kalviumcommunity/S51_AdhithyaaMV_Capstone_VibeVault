53a0f05d9515185022eac01bc7095f8faf78f62b
adhithyaa <adhthyaa.mv@kalvium.community>
2024-03-19 14:14:22 +0530

    homepage/navbar changes

--- frontend/VibeVault/src/App.jsx
+++ frontend/VibeVault/src/App.jsx
@@ -4,15 +4,18 @@ import "./App.css"
 function Navbar() {
   return (
     <div>
-      <h1 style={{ textAlign: 'center' }}>Vibe Vault</h1>
+      
       <div className="navbar">
         <a href="#">Home</a>
         <a href="#">Login</a>
         <a href="#">Sign Up</a>
         <div className="search-container">
-          <input type="text" placeholder="Search.." />
+          <input type="text" placeholder="Search" />
         </div>
+       
+
       </div>
+      <h1>VIBE VAULT</h1>
     </div>
   );
 }
