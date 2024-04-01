c56f87a4cad728ec68eecfc6b15f278c57bcc770
adhithyaa <adhthyaa.mv@kalvium.community>
2024-04-01 11:29:53 +0530

    overall-changes

---
+++ frontend/VibeVault/src/Components/Moods/HappyLang.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "../Moods/MoodLang.css"
+
+
+function Happy() {
+    return(
+        <div className='opt'>
+            <h1>Language</h1>
+            <div className='langopt'>
+                <div className='tamil'>Tamil</div>
+                <div className='telugu'>Telugu</div>
+                <div className='eng'>English</div>
+            </div>
+        </div>
+    )
+    
+}
+
+export { Happy };
---
+++ frontend/VibeVault/src/Components/Moods/RelaxedLang.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "../Moods/MoodLang.css"
+
+
+function Relaxed() {
+    return(
+        <div className='opt'>
+            <h1>Language</h1>
+            <div className='langopt'>
+                <div className='tamil'>Tamil</div>
+                <div className='telugu'>Telugu</div>
+                <div className='eng'>English</div>
+            </div>
+        </div>
+    )
+}
+
+export { Relaxed };
